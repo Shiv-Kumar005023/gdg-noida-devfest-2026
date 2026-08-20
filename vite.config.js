4a7855ae4954c268a4da45f90d5e1b680e1035fd
Shiv Kumar <cshivkumar722@gmail.com>
2026-08-20 22:30:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/gdg-noida-devfest-2026/',
   plugins: [
     react(),
     tailwindcss(),
